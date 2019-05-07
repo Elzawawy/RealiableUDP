ce21b05ab9fd8f045cff203c16433ec33649b133
Elzawawy <amr22.elzawawy@gmail.com>
2019-05-07 16:07:48 +0200

    Added RUDP un implemented child for parent socket

---
+++ RUDPSocket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zawawy on 5/7/19.
+//
+
+#include "RUDPSocket.h"
---
+++ RUDPSocket.h
@@ -0,0 +1,14 @@
+//
+// Created by zawawy on 5/7/19.
+//
+
+#ifndef REALIABLEUDP_RUDPSOCKET_H
+#define REALIABLEUDP_RUDPSOCKET_H
+
+
+class RUDPSocket {
+
+};
+
+
+#endif //REALIABLEUDP_RUDPSOCKET_H
