aa2ac1b966838fee75c6752a01a61051b13ee26c
Elzawawy <amr22.elzawawy@gmail.com>
2019-05-08 13:33:45 +0200

    Added Mutex and Condition Variables to send in order packets.

--- RUDPSocket.cpp
+++ RUDPSocket.cpp
@@ -2,9 +2,23 @@
 // Created by zawawy on 5/7/19.
 //
 
+#include <vector>
+#include <thread>
 #include "RUDPSocket.h"
 
+
+
 void RUDPSocket::Send(string& message) {
+    //Break the message into an array of packets to be sent.
+    vector<Packet> packets;
+    for (Packet pkt: packets) {
+        //create a thread for each packet.
+        thread th(&RUDPSocket::sendpkt_th,this, pkt);
+        //detach the thread from main thread.
+        th.detach();
+    }
+
+
 
 
 }
@@ -20,4 +34,12 @@ RUDPSocket::RUDPSocket(int send_maxsize) {
     this->next_seq_num_ = 1;
     // Initialize Sending sliding window maximum size.
     this->sendwind_size_ = send_maxsize;
+}
+
+void *RUDPSocket::sendpkt_th(RUDPSocket::Packet &packet, int th_id) {
+    std::unique_lock<std::mutex> lock(mtx_);
+    while(th_id != current_thid_ || !ready_run_ )
+        run_cv_.wait(lock);
+
+    return nullptr;
 }
--- RUDPSocket.h
+++ RUDPSocket.h
@@ -6,6 +6,8 @@
 #define RUDPSOCKET_H
 
 #include <string>
+#include <mutex>
+#include <condition_variable>
 #include "Socket.h"
 
 using namespace std;
@@ -17,9 +19,24 @@ public:
     void Receive(string &message, int max_length) override;
 
 private:
+    typedef struct packet {
+        /*Header*/
+        unsigned short checksum;  // 16 bits
+        unsigned short length;    // 16 bits
+        unsigned int seqno;      // 32 bits
+        /*Data*/
+        char data[500];
+
+    }Packet;
+    void* sendpkt_th(Packet &packet, int th_id);
     int base_;
     int next_seq_num_;
     int sendwind_size_;
+    mutex mtx_;                 // mutex for critical section
+    condition_variable run_cv_;  // condition variable for critical section
+    condition_variable recv_run_;
+    bool ready_run_ = false;
+    int current_thid_ = 0;
 };
 
 
