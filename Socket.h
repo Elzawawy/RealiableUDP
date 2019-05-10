862afeeeb77d5fcfa79dda3308bc1f2fac978d3f
Elzawawy <amr22.elzawawy@gmail.com>
2019-05-11 01:21:18 +0200

    Socket Interface

--- Socket.h
+++ Socket.h
@@ -1,10 +1,9 @@
 //
 // Created by zawawy on 5/1/19.
 //
-
-
-#include <string>
+#include <string.h>
 #include <sys/socket.h>
+#include <string>
 
 using namespace std;
 
@@ -17,6 +16,8 @@ public:
     typedef enum ip_version { ipv4=AF_INET,ipv6=AF_INET6,unspecified=AF_UNSPEC}ip_version;
     virtual void Send(string &message) = 0;
     virtual void Receive(string &message, int max_length) = 0;
+    virtual void Send(string &message,struct sockaddr_storage storage)=0;
+    virtual void Receive(string &message, int max_length,struct sockaddr_storage &storage)=0;
 };
 
 
